a9e96042d39994cb46d21fa27cac283885d24b9d
Viktor1907 <vikvelik@bigmir.net>
2024-02-02 19:32:27 +0200

    yask2end

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,25 @@
+class Storage {
+  #items;
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
+  }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
